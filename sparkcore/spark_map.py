a0527aea7fed2e804edf8465775b8da75f70f36c
Debabrata Patnaik <debabrata_patnaik@live.com>
2024-09-09 14:16:20 +0530

    formatted code.

--- sparkcore/spark_map.py
+++ sparkcore/spark_map.py
@@ -1,21 +1,32 @@
 from pyspark import SparkContext, SparkConf
 from pprint import pp
+
 # conf = SparkConf().setAppName("deb-spark").setMaster("local[*]")
-sc = SparkContext(master="local",appName="deb-spark")
+sc = SparkContext(master="local", appName="deb-spark")
+
 
 def read_file():
     rdd = sc.textFile("c:/test/first.txt")
 
     for ln in rdd.collect():
         pp(ln)
-isEven = lambda x: x%2==0        
-def filter_rdd():
-    rdd = sc.parallelize([5,8,9,10,11,56])
-    rdd2=rdd.filter(isEven)
-    pp([ln for ln in rdd2.collect()])
-        
 
-    
+
+isEven = lambda x: x % 2 == 0
+
+
+def filter_rdd():
+    rdd = sc.parallelize([5, 8, 9, 10, 11, 56])
+    rdd2 = rdd.filter(isEven)
+    print_rdd(rdd2)
+    rdd3 = rdd.filter(lambda n: n % 2 == 0)
+    print_rdd(rdd3)
+    print_rdd(rdd1.filter(lambda n: n >= 10))
+
+
+def print_rdd(rdd):
+    pp([ln for ln in rdd.collect()])
+
+
 if __name__ == "__main__":
     filter_rdd()
-    
