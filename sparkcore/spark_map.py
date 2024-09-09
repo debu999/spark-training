def116281bf26903a48d1affe36b5ee48a019f13
Debabrata Patnaik <debabrata_patnaik@live.com>
2024-09-09 14:35:51 +0530

    few extra operations.

--- sparkcore/spark_map.py
+++ sparkcore/spark_map.py
@@ -1,8 +1,12 @@
 from pyspark import SparkContext, SparkConf
 from pprint import pp
 
+
 # conf = SparkConf().setAppName("deb-spark").setMaster("local[*]")
 sc = SparkContext(master="local", appName="deb-spark")
+sc._jsc.hadoopConfiguration().set(
+    "mapreduce.input.fileinputformat.input.dir.recursive", "true"
+)
 
 
 def read_file():
@@ -21,12 +25,29 @@ def filter_rdd():
     print_rdd(rdd2)
     rdd3 = rdd.filter(lambda n: n % 2 == 0)
     print_rdd(rdd3)
-    print_rdd(rdd1.filter(lambda n: n >= 10))
+    print_rdd(rdd.filter(lambda n: n >= 10))
+    print_rdd(rdd.filter(lambda n: n >= 10).map(lambda x: x * x))
 
 
 def print_rdd(rdd):
     pp([ln for ln in rdd.collect()])
 
 
+def text_rdd_operations():
+    linesRdd = sc.textFile("c:/spark-training/samples")
+    pp(linesRdd.collect())
+    pp(
+        linesRdd.filter(lambda line: line.startswith("s"))
+        .map(lambda line: line.upper())
+        .collect()
+    )
+    pp(
+        linesRdd.filter(lambda line: not line.startswith("s"))
+        .map(lambda line: line.upper())
+        .collect()
+    )
+
+
 if __name__ == "__main__":
-    filter_rdd()
+    # filter_rdd()
+    text_rdd_operations()
